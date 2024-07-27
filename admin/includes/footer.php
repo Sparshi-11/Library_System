4f02a19b68d1d22e154d237b9155c07f574ddc97
Sparshi-11 <129417700+Sparshi-11@users.noreply.github.com>
2024-07-27 18:46:47 +0530

    deploy

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Books@BV |Designed by:BTI_G47
+                </div>
+
+            </div>
+        </div>
+    </section>
